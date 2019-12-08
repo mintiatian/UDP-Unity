8e8d4a0c8cf01aa0f14c57adbe5a6319cfa137c3
ちあ🐘 <mintiatian@gmail.com>
2019-12-08 10:22:21 +0900

    add:GameController

---
+++ Assets/Scenes/GameScene/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject networkGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(networkGameObject, new Vector3(0.0f, 2.0f, 0.0f), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
